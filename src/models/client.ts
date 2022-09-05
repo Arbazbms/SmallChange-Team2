73e8598aad711e5fd80b23136269c7e79a4d46d7
MVRanjitha <ranjithamva@gmail.com>
2022-09-05 15:12:43 +0530

    mocking acct data

---
+++ src/models/client.ts
@@ -0,0 +1,8 @@
+export interface Client {
+    clientId : string,
+        email:string,
+        DOB : string,
+        country: string,
+        postalCode:string,
+        identification:[string,string]
+  }
