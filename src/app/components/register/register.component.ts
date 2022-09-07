7bb2918a3acec4279d7a200885130e8bd436f53f
ashharmohammad1 <ashharmohammad1@gmail.com>
2022-09-07 20:57:12 +0530

    login form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  usernameErrorTextmsg: string =
+  'Invalid Username- Must contain between 3 and 18 letters, numbers, underscores or hyphens ';
+passwordErrorTextmsg: string =
+  'Invalid Password- Must contain between 6 and 24 letters, numbers, underscores or hyphens ';
+loginForm: FormGroup;
 
-  constructor() { }
+constructor(private formBuilder: FormBuilder, ) {
 
-  ngOnInit(): void {
-  }
+  this.loginForm = this.formBuilder.group({
+    password1: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(18),
+        Validators.pattern("^[a-zA-Z0-9_\-]{3,18}$")
+      ],
+    ],
+    password2: [
+      '',
+      [
+        Validators.required,
+        confirmpassword
+      ],
+    ],
+  });
+}
+function confirmpassword(params:type) {
+  
+}
 
+ngOnInit(): void {}
+
+get username() {
+  return this.loginForm.get('username');
+}
+
+get password(){
+  return this.loginForm.get('password')
+}
+
+onSubmit() {
+  console.log(this.loginForm.value);
+}
 }
