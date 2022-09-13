5ba7ece81256f36360aa06d75496652d4b6a5e43
ashharmohammad1 <ashharmohammad1@gmail.com>
2022-09-13 13:11:43 +0530

    Portfolio Page

--- src/app/models/portfolio.model.ts
+++ src/app/models/portfolio.model.ts
@@ -1,13 +1,21 @@
 export class Portfolio {
-    public instrument: string
-    public costprice: number 
-    public marketprice:number
+
     public gain: number
     public ifgain:boolean 
-    constructor(){
-        this.instrument=''
-        this.costprice=0
-        this.marketprice=0
+    constructor(
+        public instrument: string,
+        public instrumentid: string,
+        public orderid: string,
+        public costprice: number, 
+        public marketprice:number
+
+
+    ){
+        // this.instrument=''
+        // this.instrumentid=''
+        // this.orderid=''
+        // this.costprice=0
+        // this.marketprice=0
         this.gain=this.marketprice-this.costprice
         this.ifgain=this.marketprice>this.costprice
     }
