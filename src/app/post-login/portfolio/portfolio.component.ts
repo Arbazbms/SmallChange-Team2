5463131770da96e1bc851c783b04cd655355b1dc
JaanhaviS16 <jaanhavis16@gmail.com>
2022-09-14 01:20:45 +0530

    Resolved errors while loadin sell component

--- src/app/post-login/portfolio/portfolio.component.ts
+++ src/app/post-login/portfolio/portfolio.component.ts
@@ -10,89 +10,108 @@ import { PortfolioService } from '../services/portfolio.service';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
+  styleUrls: ['./portfolio.component.css'],
 })
-export class PortfolioComponent implements OnInit, OnDestroy{
-
-  public portfolio:Portfolio[]=[]
-  @ViewChild('dataTable')table:any;
-  dataTable:any;
-  dtOptions:DataTables.Settings = {};
+export class PortfolioComponent implements OnInit, OnDestroy {
+  public portfolio: Portfolio[] = [];
+  @ViewChild('dataTable') table: any;
+  dataTable: any;
+  dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject<any>();
-  constructor(public ps: PortfolioService) { }
-  order: Order = new Order('',-1,-1,'','','')
-  
+
+  constructor(public ps: PortfolioService) {}
+
+  instrumentSymbol: string = ''
+  order: Order = new Order('', -1, -1, '', '', '');
+
   ngOnInit(): void {
-
-    this.getallportfolio()
-    console.log(this.portfolio)
-    this.ps.getInstrument('DIS')
-
+    this.getallportfolio();
+    console.log(this.portfolio);
+    //this.ps.getInstrument('Disney');
   }
-  getallportfolio(){
-    this.ps.getPortfolio().subscribe(data=>{
-      this.portfolio=data
+
+  displaySellTab(ins:string) {
+    console.log('HELOO');
+
+    this.instrument = this.ps.getInstrument(ins);
+    this.order.quantity = this.instrument.instrument.minQuantity;
+    this.order.direction = 'S';
+    this.showModal = true;
+    console.log('in display', this.instrument);
+  }
+
+  getallportfolio() {
+    this.ps.getPortfolio().subscribe((data) => {
+      this.portfolio = data;
       this.dtTrigger.next(this.table);
       this.dtOptions = {
-        data:this.portfolio,
-        paging:true,
-        ordering:true,
+        data: this.portfolio,
+        paging: true,
+        ordering: true,
         responsive: true,
-        columnDefs:[{"defaultContent": "-",'targets': [4], /* column index */
-        'orderable': false, /* true or false */},
-                     {"targets": "_all","searchable" :true}],
-        searching:true,
-     
-         columns:[
-                  {title:'Stock',data:'instrument'},
-                  {title:'CostPrice',data:'costprice'},
-                  {title:'MarketPrice',data:'marketprice'},
-                  {title:'Gain/Loss',data:'gain'},
-                  {
-                    defaultContent: "<button class='showIdButton myclass btn-success' style=padding:5px;margin-left:10%;>Sell </button>"
-                  }
-                  ]
-  
-      };
-           
-     this.dataTable=$(this.table.nativeElement);
-     this.dataTable.DataTable(this.dtOptions);
-  
-  
+        columnDefs: [
+          {
+            defaultContent: '-',
+            targets: [4] /* column index */,
+            orderable: false /* true or false */,
+          },
+          { targets: '_all', searchable: true },
+        ],
+        searching: true,
 
+        columns: [
+          { title: 'Stock', data: 'instrument' },
+          { title: 'CostPrice', data: 'costprice' },
+          { title: 'MarketPrice', data: 'marketprice' },
+          { title: 'Gain/Loss', data: 'gain' },
+          {
+            defaultContent:
+              "<button class='showIdButton myclass btn-success' style=padding:5px;margin-left:10%;>Sell </button>",
+          },
+        ],
+      };
+
+      this.dataTable = $(this.table.nativeElement);
+      this.dataTable.DataTable(this.dtOptions);
     }),
-    this.dtTrigger.next(1);
+      this.dtTrigger.next(1);
+    var self = this
+    $('div div table tbody').on('click', 'tr', function () {
+      console.log(
+        'helllo', this.getElementsByTagName('td')[0].innerHTML,
+      );
+      this.instrumentSymbol =this.getElementsByTagName('td')[0].innerHTML
+      // this.instrument = this.ps.getInstrument('Disney');
+      // this.order.quantity = this.instrument.instrument.minQuantity;
+      // this.order.direction = 'S';
+      // this.showModal = true;
+      //console.log('in display', this.showModal);
+      self.displaySellTab(this.instrumentSymbol)
+    });
   }
 
   showModal: boolean = false;
-  instrument: Price = new Price('',-1,-1,new Date(), new Instrument('','','','','',-1,-1))
+  instrument: Price = new Price(
+    '',
+    -1,
+    -1,
+    new Date(),
+    new Instrument('', '', '', '', '', -1, -1)
+  );
+
   
 
-  displaySellTab(instrumentId : string){
-    console.log("HELOO");
-    
-    this.instrument = this.ps.getInstrument(instrumentId)
-    this.order.quantity = this.instrument.instrument.minQuantity;
-    this.order.direction = 'S'
-    this.showModal = true;
-    console.log('in display', this.instrument);
-
-  }
-
-  display(){
-    console.log("Hellp");
-    
+  display() {
+    console.log('Hellp');
   }
   ngOnDestroy(): void {
     this.dtTrigger.unsubscribe();
   }
-  submit(){
-    this.getallportfolio
+  submit() {
+    this.getallportfolio;
   }
 
-  hideDialog(show : boolean){
-    this.showModal = show
+  hideDialog(show: boolean) {
+    this.showModal = show;
   }
-  
 }
-
