967bc038222ec79b04f965f6a3fd1d07247802a8
ashharmohammad1 <ashharmohammad1@gmail.com>
2022-09-14 11:52:47 +0530

    some testing and routing

--- src/app/post-login/portfolio/portfolio.component.spec.ts
+++ src/app/post-login/portfolio/portfolio.component.spec.ts
@@ -1,14 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { DataTablesModule } from 'angular-datatables';
 import { PortfolioComponent } from './portfolio.component';
-
+// import "datatables.net";
 describe('PortfolioComponent', () => {
   let component: PortfolioComponent;
   let fixture: ComponentFixture<PortfolioComponent>;
 
   beforeEach(async () => {
+    [
+    
+      {
+        instrumentId: 'AMZN',
+        bidPrice: 100,
+        askPrice: 200,
+        timeStamp: new Date('12/10/2006'),
+        instrument: {
+          instrumentId: 'AMZN',
+          description: 'Amazon.com',
+          externalIdType: 'ISIN',
+          externalId: 'ISIN14577',
+          categoryId: 'MainIndex',
+          minQuantity: 10,
+          maxQuantity: 130,
+        },
+      },
+      {
+        instrumentId: 'DIS',
+        bidPrice: 5,
+        askPrice: 3,
+        timeStamp: new Date('12/10/2006'),
+        instrument: {
+          instrumentId: 'DIS',
+          description: 'Disney',
+          externalIdType: 'ISIN',
+          externalId: 'ISIN01682',
+          categoryId: 'MainIndex',
+          minQuantity: 2,
+          maxQuantity: 39,
+        },
+      }
+    let portfolioService: any= jasmine.createSpyObj('PortfolioService',['getPortfolio']);
+    portfolioService.getportfolios.and.returnValue( of(testportfolios));
     await TestBed.configureTestingModule({
-      declarations: [ PortfolioComponent ]
+      declarations: [ PortfolioComponent ],
+      imports:[DataTablesModule]
     })
     .compileComponents();
   });
