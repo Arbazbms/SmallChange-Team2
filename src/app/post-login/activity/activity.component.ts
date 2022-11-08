4b92ed20d237ea087d9051c3ff82790df9309215
JaanhaviS16 <jaanhavis16@gmail.com>
2022-11-09 02:23:41 +0530

    Portfolio get operation intergration

--- src/app/post-login/activity/activity.component.ts
+++ src/app/post-login/activity/activity.component.ts
@@ -10,6 +10,8 @@ import { Subject, take } from 'rxjs';
 import { TradeHist } from '../models/trade-hist';
 import { TradeHistory } from '../services/tradeHistory.service';
 import { Trade } from 'src/app/models/trade';
+import { Instrument } from 'src/app/models/instrument';
+import { Price } from 'src/app/models/price';
 
 declare var $: any;
 @Component({
@@ -21,10 +23,31 @@ export class ActivityComponent implements OnInit {
   public tradeHist: TradeHist[] = [];
   public trade: Trade[] = []
   private clientId : any = localStorage.getItem('client');
+  instrument: Price = new Price('',-1,-1,new Date(), new Instrument('','','','','',-1,-1))
+  instruments: Price[] = [];
   constructor(private tradeHistoryService: TradeHistory, private tradeService: TradeService) {}
 
   ngOnInit(): void {
     this.getAllTradeHistory();
+    this.getInstruments();
+  }
+
+  getInstruments() {
+    this.tradeService.getAllInstruments().subscribe((data) => {
+      this.instruments = <Price[]>data;
+      console.log(this.instruments);
+    });
+  }
+  
+  getInstrumentNameById(instrumentId : string): string{
+    let name = ""
+    Object.entries(this.instruments).forEach(([key, element], index) => {
+      if( element.instrumentId === instrumentId){
+        console.log(element.instrument.instrumentDescription);
+        name = element.instrument.instrumentDescription
+      }
+    });
+    return name
   }
 
 
--- src/app/post-login/services/trade.service.ts
+++ src/app/post-login/services/trade.service.ts
@@ -11,67 +11,54 @@ import { TradeModel } from 'src/app/models/trade-model';
   providedIn: 'root',
 })
 export class TradeService {
-  cashBalance: number = 1000000;
-  //dbUrl: string = 'http://localhost:8080/trades'
+  dbUrl: string = 'http://localhost:8080/trades'
   baseUrl: string = 'http://localhost:4000/';
   fmtsUrl: string = 'http://localhost:3000/fmts/trades/'
-  tradeRef = 3463256445;
-  intrumentDetails: TradeModel[] = [];
+  
   price: Price = new Price('',-1, -1,new Date(),new Instrument('', '', '', '', '', -1, -1));
-  priceDetails: Price[] = [];
+  instruments: Price[] = [];
 
   tradeOrderMap : any = {
     order: {}, trade: {}
   }
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
+
+  
 
   getAllInstruments() : Observable<Price[]>{
-    
     return this.http.get<Price[]>(this.fmtsUrl+'prices')
   }
 
-  dbUrl: string = 'http://localhost:8080/trades'
   getAllTradeByClientId(clientId : string) : Observable<Trade[]>{
     console.log(`${this.dbUrl}/tradeClient/${clientId}`)
-    clientId = "C101"
     return this.http.get<Trade[]>(`${this.dbUrl}/tradeClient/${clientId}`)
+  }
+
+  getInstrumentNameById(instrumentId : string): string{
+    let name = ""
+    Object.entries(this.instruments).forEach(([key, element], index) => {
+      if( element.instrumentId === instrumentId){
+        console.log(element.instrument.instrumentDescription);
+        name = element.instrument.instrumentDescription
+      }
+    });
+    return name
   }
 
   headers = new HttpHeaders({
     'content-type' : 'application/json'
   })
 
-  saveOrder(orderObj:Order): any{
+  saveOrder(orderObj:Order) :Observable<Trade[]>{
     console.log(`${this.dbUrl}/order}`)
-    return this.http.post( `${this.dbUrl}/order`, orderObj,  { headers: this.headers })
-    // return this.http.post( `${this.usersUrl}/add`,newConfig);
-  
+    return this.http.post<Trade[]>( `${this.dbUrl}/order`, orderObj,  { headers: this.headers })
   }
 
-  saveTrade(trade: Trade){
+  saveTrade(trade: Trade):Observable<Trade[]>{
     console.log("inside trade service**"+ JSON.stringify(trade))
-    this.http.post( 'http://localhost:8080/trades/trade',trade, { headers: this.headers } )
+    return this.http.post<Trade[]>( 'http://localhost:8080/trades/trade',trade, { headers: this.headers } )
     
   }
 
-  saveOrderandTrade(orderObj:Order, tradeObj:Trade): Observable<Trade[]>{
-    this.tradeOrderMap.order = orderObj
-    this.tradeOrderMap.trade = tradeObj
-    console.log("POST Spring: ",  JSON.stringify(this.tradeOrderMap));
-    return this.http.post<Trade[]>(this.dbUrl, JSON.stringify(this.tradeOrderMap))
-  }
-
-  placeOrder(order: Order): number {
-    if (this.checkCash()) {
-      this.http.post(this.baseUrl + 'order', order).subscribe();
-      this.cashBalance = this.cashBalance - order.targetPrice;
-      return this.tradeRef++;
-    }
-    return 0;
-  }
-
-  /* Define Function */
-  checkCash(): boolean {
-    return true;
-  }
 }
